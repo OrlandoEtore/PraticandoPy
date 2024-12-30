11ff95e9c59d931cf220dd26e9039540ea918c0f
OrlandoEtore <orlando.etore@gmail.com>
2024-12-29 21:09:58 -0300

    Att

---
+++ PraticarMemMasc/cp006.py
@@ -0,0 +1,23 @@
+
+
+# x = 2
+# y = 3
+
+# print(x+y)
+
+# nome = "Orlando"
+# primeiro = "Orlando"
+# sobre = "Etore"
+# tutorial = str(8.0)
+
+# print(nome + ' ' +primeiro + " "+sobre + " " +tutorial)
+
+nome = "Orlando"
+
+print(nome)
+print(nome[0:5])
+print(nome[0:])
+print(nome[-1])
+print(nome[:-2])
+print(nome[::-1])
+print(nome[0::])
