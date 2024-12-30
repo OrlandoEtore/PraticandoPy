77323793046ed7c9485992c16c33328cafb10afd
OrlandoEtore <orlando.etore@gmail.com>
2024-12-29 21:52:23 -0300

    Att

---
+++ PraticarMemMasc/cp007.py
@@ -0,0 +1,31 @@
+
+# a = 1
+# print(type(a))
+
+# b = 1.5
+# print(type(b))
+
+# c = True
+# d = False
+
+# print(type(c))
+# print(type(d))
+
+# e = 1+ 1j
+# print(type(e))
+
+string = "Orlando"
+print(type(string))
+
+for1 = range(5)
+print(type(for1))
+
+# Ele bacana ele duplicadas 
+sets = {1,2,3,4,5}
+print(type(sets))
+
+list = [1,2,3,4,5]
+print(type(list))
+
+
+
