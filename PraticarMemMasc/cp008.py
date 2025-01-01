452f0935e65268f59aa4bd35aa12bfd2a9533b23
OrlandoEtore <orlando.etore@gmail.com>
2025-01-01 00:10:46 -0300

    Primeiro Codigo do Ano de 2025

---
+++ PraticarMemMasc/cp008.py
@@ -0,0 +1,87 @@
+
+
+# a = 3
+# b = 2
+
+# print("O valor ",a)
+# print("O valor ",b)
+# print(3+2)
+
+# print("A soma  A + B = ",a+b)
+# print("A sub =",a-b)
+# print("A divisao = ",a/b)
+# print("A multiplicao = ",a*b)
+
+# a = 5
+# a *= 2
+# print(a)
+
+# x = 1
+# y = 2
+
+# x,y = 1,2
+
+# print("O valor de X = ",x)
+# print("o valor de Y = ",y)
+
+
+# Relacao 
+# a = 1
+# b = 2
+# c = 3
+
+# print(a < b)
+# print(a > b)
+# print(a == b)
+# print(a == c)
+# print(a == a)
+# print(a <= b)
+# print(a >= b)
+# print(a <= c)
+# print(a != b)
+# priny(a != c)
+
+
+# Logica
+
+a = 1
+b = 2
+
+# And 
+print( a < 5 and b < 5)
+print( a < 5 and b > 5)
+
+# OU 
+print(a < 5 or b < 5)
+print(a < 5 or b > 5)
+print(a > 5 or b > 5)
+
+# Not
+
+x = True
+print(x)
+print(type(x))
+print(not x)
+y = not x
+print(y)
+a = 1
+b = 2
+print(a == b)
+print(a != b)
+
+# Unary
+
+a = 1
+print(a)
+print(-a)
+c = 2
+
+
+c = -b
+print(b)
+print(c)
+
+
+
+
+
---
+++ PraticarMemMasc/cp009.py
@@ -0,0 +1,31 @@
+
+
+
+a = int(input("Numero  inteiro :"))
+b = int(input("Numero Inteiro :"))
+c = a + b
+
+print("O Primeiro Valor do user e = ",a)
+print("O Segundo Valor do  user e = ",b)
+print("A soma dos Valores = ",c)
+
+print(type(a))
+print(type(b))
+
+a = input("Valor ")
+b = input("Valor ")
+
+c = a + b
+
+print(a+b)
+print(c)
+print(c.upper())
+
+
+# Expressaoes
+
+final = eval(input("Entre com a espressao Escrita :"))
+print(final)
+
+
+
