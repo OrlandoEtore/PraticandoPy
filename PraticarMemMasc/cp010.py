e7e5240b7080dadc92b292f9349edfabad5bf682
OrlandoEtore <orlando.etore@gmail.com>
2025-01-04 22:05:04 -0300

    Add

---
+++ PraticarMemMasc/cp010.py
@@ -0,0 +1,100 @@
+
+
+
+# a = 3
+# b = 2
+
+# # print("O Valor A :",a)
+# # print("O valor B :",b)
+# # print(3+2)
+# # print("Addicao :",a+b)
+# # print("Sub :",a-b)
+# # print("Div : ",a/b)
+# # print("Multiplicao : ",a*b)
+
+# a = a + 1
+# print(a)
+# b += 1
+# print(b)
+
+# a = 3
+# a *= 2
+# # print(a)
+
+# x = 1
+# y = 2
+
+# x,y = 1,2
+# print("O valor de X : ",x)
+# print("O valor de Y : ",y)
+
+# # Relacao
+
+# a = 1
+# b = 2
+# c = 3
+
+# print(a < b)
+# print(a > b)
+# print(a == b)
+# print(a == c)
+# print(a == a)
+# print(a <= b)
+# print(a >= b)
+# print(a <= c)
+# print(a != b)
+# print(a != c)
+
+#logica
+
+a = 1
+b = 2
+
+#And
+print(a < 5 and b < 5)
+print(a < 5 and b > 5)
+
+#OR
+
+print(a < 5 or b < 5)
+print(a < 5 or b > 5)
+print(a > 5 or b > 5)
+
+#Not
+x = True
+print(x)
+print(type(x))
+print(not x)
+y = not x 
+print(y)
+a = 1 
+b = 2
+print(a == b)
+print(a != b)
+
+#Unary
+
+a = 1
+print(a)
+print(-a)
+b = 2
+c = -b
+print(b)
+print(c)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
