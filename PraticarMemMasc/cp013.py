46db7932e5fc925c83a2857bc06c60642534222f
OrlandoEtore <orlandoetore@gmail.com>
2025-01-13 20:18:37 -0300

    Add

--- PraticarMemMasc/cp013.py
+++ PraticarMemMasc/cp013.py
@@ -1,3 +1,45 @@
 # lista
 
 colegas = ["Ana","Pedro","Mike","Alex"]
+# print(colegas)
+# print(type(colegas))
+
+# print(colegas[3])
+# colegas[1] = "Alex "
+# print(colegas)
+
+colegas.append("Jaoa")
+# print(colegas)
+
+colegas.append(10)
+# print(colegas)
+
+colegas.append(101.9)
+# print(colegas)
+
+colegas.append(True)
+# print(colegas)
+
+# print(colegas[2])
+# colegas.insert(2,"Mike")
+# print(colegas)
+
+
+# print(colegas[2])
+# colegas.remove("Jaoa")
+# print(colegas)
+
+nos = [0,1,2,3,4,5,8,11,98,45,24,32,99,100]
+# print("O Tamanho da lista e :.2f",nos)
+# print("O Tamanho da lista e :.2f",len(nos))
+# # print(min(nos))
+# print(max(nos))
+# nos.sort()
+# print("\n",nos)
+# nos.reverse()
+# print(nos)
+
+print(nos)
+nos.pop()
+nos.pop()
+print(nos)
