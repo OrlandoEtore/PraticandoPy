cb2faa16d92651719db7f74861ebe270c99d7225
OrlandoEtore <orlandoetore@gmail.com>
2025-01-14 20:20:03 -0300

    Att

--- PraticarMemMasc/cp014.py
+++ PraticarMemMasc/cp014.py
@@ -1,2 +1,13 @@
+lista = [1,2,3,4,5,66,88,99,101,120,875,981,997]
 
-print("Alterecao")
+# print(lista)
+# print(lista[0:100])
+# print(lista[1:])
+# print(lista[:6])
+# print(lista[6])
+
+print(lista[-1])
+print(lista[-5])
+print(lista[-8:-5])
+print(lista[::4])
+print(lista[::-1])
---
+++ PraticarMemMasc/cp015.py
@@ -0,0 +1,8 @@
+# lista com loop
+
+for i in range(0,10):
+    print(" ",i)
+
+# no =int(input())
+# for i in range(0,no):
+#     print(i)
