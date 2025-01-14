2c4088e6d097c43eb9492f6e64160550e309123c
OrlandoEtore <orlandoetore@gmail.com>
2025-01-14 20:53:52 -0300

    Att

--- PraticarMemMasc/cp015.py
+++ PraticarMemMasc/cp015.py
@@ -1,8 +1,29 @@
 # lista com loop
 
-for i in range(0,10):
-    print(" ",i)
+# for i in range(0,10):
+#     print(" ",i)
 
 # no =int(input())
 # for i in range(0,no):
 #     print(i)
+
+# lista = ['item01','item02','item03']
+# for i in lista:
+#     print(i)
+
+# lista = ['item01','item02','item03']
+# id = [[1,2,3],[4,5,6],[7,8,9]]
+
+# for item in id:
+#     for i in item:
+#         print(i) 
+
+lista = []
+
+n = int(input("O numero de elementos :"))
+
+for i in range(0,n):
+    elementos = int(input())
+    list.append(elementos)
+print(list)
+
